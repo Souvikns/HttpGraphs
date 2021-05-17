e450b6322dfb1b35a75cd181caff6463f712e36c
Souvik <souvikde.ns@gmail.com>
2021-05-17 12:06:52 +0530

    inital setup

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,11 +1,17 @@
 import nextConnect from 'next-connect';
+import { generateGraph } from '../../lib';
 
 const handler = nextConnect();
 
 handler
-    .get((req,res) => {
-        console.log(req.query);
-        res.end("data");
+    .get((req, res) => {
+        let { data, graph } = req.query;
+        // graph = "line" | "bar"
+        let svgGraph = generateGraph({
+            graph,
+            data
+        });
+        res.end(svgGraph);
     })
 
 export default handler;
