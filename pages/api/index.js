fc3dd0d6ae4adc7ac02c3949f2bdcc464a355a0c
Souvik <souvikde.ns@gmail.com>
2021-06-02 22:00:01 +0530

    Update index.js

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,15 +1,10 @@
 import nextConnect from 'next-connect';
-import { generateGraph } from '../../lib';
 
 const handler = nextConnect();
 
 handler
     .get((req, res) => {
-        let { config } = req.query;
-        // graph = "line" | "bar"
-        let svgGraph = generateGraph(JSON.parse(config));
-        res.setHeader("content-type", "image/svg+xml");
-        res.end(svgGraph);
+        res.end("Hello")
     })
 
 export default handler;
