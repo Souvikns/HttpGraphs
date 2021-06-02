eacbc9db6a922a5eb5feb9ef7f9828e57c507f4a
Restyled.io <commits@restyled.io>
2021-06-02 16:31:20 +0000

    Restyled by clang-format

--- pages/api/index.js
+++ pages/api/index.js
@@ -2,9 +2,6 @@ import nextConnect from 'next-connect';
 
 const handler = nextConnect();
 
-handler
-    .get((req, res) => {
-        res.end("Hello")
-    })
+handler.get((req, res) => {res.end("Hello")})
 
 export default handler;
