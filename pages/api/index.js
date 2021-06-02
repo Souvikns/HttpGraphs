86aae33dac37f922e7098b99cf3a10d6ec9f3b79
Restyled.io <commits@restyled.io>
2021-06-02 16:31:25 +0000

    Restyled by prettier

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,7 +1,9 @@
-import nextConnect from 'next-connect';
+import nextConnect from "next-connect";
 
 const handler = nextConnect();
 
-handler.get((req, res) => {res.end("Hello")})
+handler.get((req, res) => {
+  res.end("Hello");
+});
 
 export default handler;
